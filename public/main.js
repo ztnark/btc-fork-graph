49e0c8022b70a1e57f4e6c6be7cbd9e25801b0cb
Jeff K <jjkrantz@gmail.com>
2017-08-16 16:26:35 +0000

    dari

--- public/main.js
+++ public/main.js
@@ -5,13 +5,19 @@ $.ajax({
         var bchRatios = [];
         var btcRatios = [];
         var dates = [];
-        _.each(response["BCH"],function(bch,index){
-            bchRatios.push((bch["profit_index"] / response["BTC"][index]["profit_index"])*100);
-            var time = moment(bch["created_at"]);
-            dates.push(time.format('LT'));
+	var bchs = _.filter(response["BCH"]["history"]["all"],function(entry){
+	    return entry["timestamp"] > 1501731000;
+	});
+        var btcs = _.filter(response["BTC"]["history"]["all"],function(entry){
+            return entry["timestamp"] > 1501731000;
         });
-        _.each(response["BTC"],function(btc,index){
-            btcRatios.push((btc["profit_index"] / response["BCH"][index]["profit_index"])*100);
+        _.each(response["BCH"]["history"]["all"],function(bch,index){
+            bchRatios.push((bch["dari"] / response["BTC"]["history"]["all"][index]["dari"])*100);
+            var time = moment(bch["timestamp"]*1000);
+            dates.push(time.format('MM-DD-YYYY'));
+        });
+        _.each(response["BTC"]["history"]["all"],function(btc,index){
+            btcRatios.push((btc["dari"] / response["BCH"]["history"]["all"][index]["dari"])*100);
         });
         var dataSets = [{data: bchRatios, backgroundColor:'rgb(255, 99, 132)', borderColor:'rgb(255, 99, 132)',fill: false, label: "BCH/BTC"}, {data: btcRatios, borderColor:'rgb(54, 162, 235)', backgroundColor:'rgb(54, 162, 235)', fill: false, label: "BTC/BCH"}];
         drawChart(dataSets,dates);
@@ -43,7 +49,7 @@ var myLineChart = new Chart(ctx, {
                                 min: 0,
                                 steps: 10,
                                 stepValue: 5,
-                                max: 200
+                                max: 500
                         }
                 }]
             }
